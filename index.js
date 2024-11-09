39d0ba5f2de2e79341a3cee205b2b6a719cb7934
sobitxon1 <f5itcenter1115@gmail.com>
2024-11-09 09:07:43 +0500

    cllas

---
+++ index.js
@@ -0,0 +1,35 @@
+// class Person {
+//     constructor (name,age,hobbe) {
+//     this.name=name
+//     this.age=age
+//     this.hobbe=hobbe
+//     }
+
+// info () {
+//     let sum =[]
+// for (let i = 0; i < this.name.length; i++) {
+//             if (+this.name[i]) {
+//                 sum.push(this.name[i])
+//             } 
+// }
+// console.log(sum);
+// }
+
+
+// Prev (){ 
+//     let res= this.age.reduce((a,b) =>{
+//         return a+b
+//     },0)
+
+//     console.log(res);
+// }
+
+// }
+
+
+// let User = new Person('234356789bhgvffghfdg',[5,6],'game')
+
+
+// User.info()
+// User.Prev()
+
